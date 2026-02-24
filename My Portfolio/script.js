0134eb0387924e354480939dc94724e0c73fd065
Ayaad Ullah Bayg <ayadullahbaig69@gmail.com>
2026-02-24 23:29:01 +0500

    Add files via upload

---
+++ My Portfolio/script.js
@@ -0,0 +1,10 @@
+// Smooth scroll for navigation links
+const links = document.querySelectorAll('nav ul li a');
+
+links.forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
